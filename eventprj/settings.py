268f0d79caf56dfad40b1b9079abfa1dcd32d39b
Okubule <topepaul38@gmail.com>
2025-09-29 14:26:35 +0100

    Fixing Email

--- eventprj/settings.py
+++ eventprj/settings.py
@@ -133,8 +133,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 EMAIL_BACKEND = config("EMAIL_BACKEND")
 EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT")
-EMAIL_USE_TLS = config("EMAIL_USE_TLS")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")  
 DEFAULT_FROM_EMAIL =config("DEFAULT_FROM_EMAIL")
